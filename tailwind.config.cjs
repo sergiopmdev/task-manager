a5675f01f7defd742db5fb8d0a81a771753cc69b
Sergio Peña <sergio.pm.dev@gmail.com>
2023-02-18 16:59:31 +0100

    [TM-O1] Add theme and general styles
    Implementation of the initial version of the application theme as well as the general styles of the application

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-blue": "#1D3557",
+      "medium-blue": "#457B9D",
+      "light-blue": "#A8DADC",
+      "off-white": "#F1FAEE",
+      "standard-red": "#E63946",
+    },
+    fontFamily: {
+      archivo: ["Archivo", "sans-serif"],
+      opensans: ["Open Sans", "sans-serif"],
+    },
   },
   plugins: [],
 };
