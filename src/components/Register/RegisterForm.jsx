36da30c5f819d780ab2ce3475b3cc14c63ddbad4
Sergio <sergio.pm.dev@gmail.com>
2023-03-11 17:48:56 +0100

    [TMA-01] Implement database registration
    Implementation of the database registration from the user registration form

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -1,23 +1,32 @@
-import { useForm } from "react-hook-form";
 import Input from "../Shared/Input";
 import FormSubmit from "../Shared/FormSubmit";
 import FormTitle from "../Shared/FormTitle";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import postUser from "../../services/postUser";
 
 function RegisterForm() {
+  const [registerStatus, setRegisterStatus] = useState();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => postUser(data, setRegisterStatus);
 
   return (
     <div className="flex h-[calc(100vh-6rem)] w-screen items-center justify-center">
       <form
-        className="mx-auto flex w-[35rem] flex-col justify-center rounded-md bg-extra-dark-blue py-8 px-10"
+        className="relative mx-auto flex w-[35rem] flex-col justify-center rounded-md bg-extra-dark-blue py-8 px-10"
         onSubmit={handleSubmit(onSubmit)}
       >
+        {registerStatus == 409 && (
+          <div className="absolute top-3 right-0 rounded-sm bg-error-color py-1 px-2 text-xs">
+            User already exists
+          </div>
+        )}
         <FormTitle title="Sign Up" />
         <Input
           label="Name"
